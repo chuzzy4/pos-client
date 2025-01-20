9cc9615b0c3720ec53f7ece27b3358b20057d07b
Your Name <chizupraise31@gmail.com>
2025-01-20 05:21:34 +0100

    switched from localhost to live

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -23,8 +23,8 @@ export const setSessionExpiredHandler = (callback: () => void) => {
 };
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 apiClient.interceptors.request.use(config => {
